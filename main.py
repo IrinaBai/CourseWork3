b26535fd75d22825ac3c77825b33a5b909cc1626
Your Name <you@example.com>
2023-07-21 04:29:27 +0600

    main.py file

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from utils import executed_operations, newest_operations, load_json
 import json
+from datetime import datetime
+
 def main():
     data = load_json()
     filtered_operations = executed_operations(data)
     newest_ops = newest_operations(filtered_operations)
 
-    newest_ops_json = json.dumps(newest_ops, indent=2)
+    # Convert date format to ДД.ММ.ГГГГ
+    for operation in newest_ops:
+        date_str = operation.get('date')
+        date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
+        operation['date'] = date_obj.strftime('%d.%m.%Y')
+
+    newest_ops_json = json.dumps(newest_ops, indent=2, ensure_ascii=False)
     print(newest_ops_json)
+
+
 
 if __name__ == "__main__":
     main()
